c7fdf6847cbe5102956de2ebb3205e6d35fcd73d
FelipeWeek066 <felipe1smg1g@gmail.com>
2024-05-04 12:44:52 -0400

    first commit

---
+++ src/test/java/com/week/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.week.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
